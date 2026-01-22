ff8b3a587becf09db5c3e310c93b2e72974bd4d3
Rob Barry <rob.barry@wsj.com>
2026-01-22 06:05:21 -0500

    velocity: gentler friction, use floats

--- mods/velocity.js
+++ mods/velocity.js
@@ -1,106 +1,115 @@
+// Velocity physics - smoother version with gentler friction
+// Pixels can have vx/vy properties that persist and decay gradually
+
 runAfterLoad(function(){
 
+var FRICTION = 0.98;  // velocity multiplier per tick (0.98 = 2% loss)
+var MIN_VELOCITY = 0.1;  // velocities below this are zeroed
+
 doVelocity = function(pixel) {
-    if ((pixel.vx||pixel.vy) && elements[pixel.element].movable) {
-        if (pixel.vx) {
-            // move the pixel vx times
-            for (var i = 0; i < Math.abs(pixel.vx); i++) {
-                var x = pixel.x+Math.sign(pixel.vx);
-                var y = pixel.y;
-                if (!tryMove(pixel,x,y)) {
-                    if (!isEmpty(x,y,true)) {
-                        var newPixel = pixelMap[x][y];
-                        if (elements[newPixel.element].movable) {
-                            newPixel.vx = (newPixel.vx||0) + pixel.vx - Math.sign(pixel.vx);
-                            if (elements[pixel.element].breakInto && Math.random()<elements[pixel.element].breakIntoChance) {
-                                changePixel(pixel,elements[pixel.element].breakInto);
-                            }
-                        }
+    if (!(pixel.vx || pixel.vy) || !elements[pixel.element].movable) return;
+
+    // Handle horizontal velocity
+    if (pixel.vx) {
+        var moveAmount = Math.floor(Math.abs(pixel.vx));
+        var direction = Math.sign(pixel.vx);
+
+        for (var i = 0; i < moveAmount; i++) {
+            var x = pixel.x + direction;
+            var y = pixel.y;
+
+            if (!tryMove(pixel, x, y)) {
+                if (!isEmpty(x, y, true)) {
+                    var newPixel = pixelMap[x][y];
+                    if (elements[newPixel.element].movable) {
+                        // Transfer momentum
+                        newPixel.vx = (newPixel.vx || 0) + pixel.vx * 0.5;
                     }
-                    pixel.vx = 0;
                 }
+                pixel.vx = 0;
+                break;
             }
-            if (pixel.vx) { pixel.vx = Math.floor(Math.abs(pixel.vx)/1.25)*Math.sign(pixel.vx) }
         }
-        if (pixel.vy) {
-            // move the pixel vy times
-            for (var i = 0; i < Math.abs(pixel.vy); i++) {
-                var x = pixel.x;
-                var y = pixel.y+Math.sign(pixel.vy);
-                if (!tryMove(pixel,x,y)) {
-                    if (!isEmpty(x,y,true)) {
-                        var newPixel = pixelMap[x][y];
-                        if (elements[newPixel.element].movable) {
-                            newPixel.vy = (newPixel.vy||0) + pixel.vy - Math.sign(pixel.vy);
-                            if (elements[pixel.element].breakInto && Math.random()<elements[pixel.element].breakIntoChance) {
-                                changePixel(pixel,elements[pixel.element].breakInto);
-                            }
-                        }
+
+        // Apply friction
+        if (pixel.vx) {
+            pixel.vx *= FRICTION;
+            if (Math.abs(pixel.vx) < MIN_VELOCITY) pixel.vx = 0;
+        }
+    }
+
+    // Handle vertical velocity
+    if (pixel.vy) {
+        var moveAmount = Math.floor(Math.abs(pixel.vy));
+        var direction = Math.sign(pixel.vy);
+
+        for (var i = 0; i < moveAmount; i++) {
+            var x = pixel.x;
+            var y = pixel.y + direction;
+
+            if (!tryMove(pixel, x, y)) {
+                if (!isEmpty(x, y, true)) {
+                    var newPixel = pixelMap[x][y];
+                    if (elements[newPixel.element].movable) {
+                        // Transfer momentum
+                        newPixel.vy = (newPixel.vy || 0) + pixel.vy * 0.5;
                     }
-                    pixel.vy = 0;
-                    return;
                 }
+                pixel.vy = 0;
+                break;
             }
-            if (pixel.vy) { pixel.vy = Math.floor(Math.abs(pixel.vy)/1.25)*Math.sign(pixel.vy) }
+        }
+
+        // Apply friction
+        if (pixel.vy) {
+            pixel.vy *= FRICTION;
+            if (Math.abs(pixel.vy) < MIN_VELOCITY) pixel.vy = 0;
         }
     }
 }
 
 runPerPixel(doVelocity);
 
-
 })
 
-explodeAt = function(x,y,radius,fire="fire") {
-    // if fire contains , split it into an array
+// Explosion function with velocity
+explodeAt = function(x, y, radius, fire="fire") {
     if (fire.indexOf(",") !== -1) {
         fire = fire.split(",");
     }
-    var coords = circleCoords(x,y,radius);
-    var power = radius/10;
-    //for (var p = 0; p < Math.round(radius/10+1); p++) {
+    var coords = circleCoords(x, y, radius);
+    var power = radius / 10;
+
     for (var i = 0; i < coords.length; i++) {
-        // damage value is based on distance from x and y
-        var damage = Math.random() + (Math.floor(Math.sqrt(Math.pow(coords[i].x-x,2) + Math.pow(coords[i].y-y,2)))) / radius;
-        // invert
+        var damage = Math.random() + (Math.floor(Math.sqrt(Math.pow(coords[i].x - x, 2) + Math.pow(coords[i].y - y, 2)))) / radius;
         damage = 1 - damage;
-        if (damage < 0) { damage = 0; }
+        if (damage < 0) damage = 0;
         damage *= power;
-        if (isEmpty(coords[i].x,coords[i].y)) {
-            // create smoke or fire depending on the damage if empty
-            if (damage < 0.02) { } // do nothing
+
+        if (isEmpty(coords[i].x, coords[i].y)) {
+            if (damage < 0.02) { }
             else if (damage < 0.2) {
-                createPixel("smoke",coords[i].x,coords[i].y);
+                createPixel("smoke", coords[i].x, coords[i].y);
             }
             else {
-                // if fire is an array, choose a random item
-                if (Array.isArray(fire)) {
-                    createPixel(fire[Math.floor(Math.random() * fire.length)],coords[i].x,coords[i].y);
-                }
-                else {
-                    createPixel(fire,coords[i].x,coords[i].y);
-                }
+                var elem = Array.isArray(fire) ? fire[Math.floor(Math.random() * fire.length)] : fire;
+                createPixel(elem, coords[i].x, coords[i].y);
             }
         }
-        else if (!outOfBounds(coords[i].x,coords[i].y)) {
-            // damage the pixel
+        else if (!outOfBounds(coords[i].x, coords[i].y)) {
             var pixel = pixelMap[coords[i].x][coords[i].y];
             var info = elements[pixel.element];
-            if (info.hardness) { // lower damage depending on hardness(0-1)
+
+            if (info.hardness) {
                 if (info.hardness < 1) {
-                    // more hardness = less damage, logarithmic
-                    damage *= Math.pow((1-info.hardness),info.hardness);
+                    damage *= Math.pow((1 - info.hardness), info.hardness);
                 }
                 else { damage = 0; }
             }
+
             if (damage > 0.9) {
-                if (Array.isArray(fire)) {
-                    var newfire = fire[Math.floor(Math.random() * fire.length)];
-                }
-                else {
-                    var newfire = fire;
-                }
-                changePixel(pixel,newfire);
+                var newfire = Array.isArray(fire) ? fire[Math.floor(Math.random() * fire.length)] : fire;
+                changePixel(pixel, newfire);
                 continue;
             }
             else if (damage > 0.25) {
@@ -109,30 +118,28 @@ explodeAt = function(x,y,radius,fire="fire") {
                     continue;
                 }
                 else {
-                    if (Array.isArray(fire)) {
-                        var newfire = fire[Math.floor(Math.random() * fire.length)];
-                    }
-                    else {
-                        var newfire = fire;
-                    }
+                    var newfire = Array.isArray(fire) ? fire[Math.floor(Math.random() * fire.length)] : fire;
                     if (elements[pixel.element].onBreak !== undefined) {
                         elements[pixel.element].onBreak(pixel);
                     }
-                    changePixel(pixel,newfire);
+                    changePixel(pixel, newfire);
                     continue;
                 }
             }
+
             if (damage > 0.75 && info.burn) {
                 pixel.burning = true;
                 pixel.burnStart = pixelTicks;
             }
-            pixel.temp += damage*radius*power;
+
+            pixel.temp += damage * radius * power;
             pixelTempCheck(pixel);
-            // set the pixel.vx and pixel.vy depending on the angle and power
+
+            // Apply explosion velocity
             if (!elements[pixel.element].excludeRandom) {
-                var angle = Math.atan2(pixel.y-y,pixel.x-x);
-                pixel.vx = Math.round((pixel.vx|0) + Math.cos(angle) * (radius * power/10));
-                pixel.vy = Math.round((pixel.vy|0) + Math.sin(angle) * (radius * power/10));
+                var angle = Math.atan2(pixel.y - y, pixel.x - x);
+                pixel.vx = (pixel.vx || 0) + Math.cos(angle) * (radius * power / 10);
+                pixel.vy = (pixel.vy || 0) + Math.sin(angle) * (radius * power / 10);
             }
         }
     }
